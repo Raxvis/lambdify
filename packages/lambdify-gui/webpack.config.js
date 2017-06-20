bb134204f5ecc08d312912cf1bdebd6a106884c9
William <william.h.giles@gmail.com>
2017-06-20 15:31:26 -0500

    webpack cleanup

--- packages/lambdify-gui/webpack.config.js
+++ packages/lambdify-gui/webpack.config.js
@@ -8,19 +8,20 @@ module.exports = {
 		'./packages/lambdify-gui/src/index.js'
 	],
 	module: {
-		loaders: [{
-			loader: 'babel-loader?presets[]=react&presets[]=stage-2',
-			test: /\.jsx?$/
-		}, {
-			include: /node_modules/,
-			loader: 'json-loader',
-			test: /\.json$/
-		}]
+		loaders: [
+			{
+				loader: 'babel-loader?presets[]=react&presets[]=stage-2',
+				test: /\.jsx?$/
+			},
+			{
+				include: /node_modules/,
+				loader: 'json-loader',
+				test: /\.json$/
+			}
+		]
 	},
 	output: { filename: './packages/lambdify-gui/dist/bundle.js' },
-	plugins: [
-		argv.env.stage === 'prod' ? new webpack.optimize.UglifyJsPlugin() : undefined
-	],
+	plugins: [argv.env.stage === 'prod' ? new webpack.optimize.UglifyJsPlugin() : undefined],
 	resolve: { alias: { utils: path.resolve('./packages/lambdify-gui/src/utils/') } },
 	target: 'electron-renderer',
 	watch: true
