e964d00c4b5a132c49f64f28164edb22f6824cf3
William <william.h.giles@gmail.com>
2018-03-22 10:19:46 -0500

    Simplify runner.js

--- packages/lambdify-runner/src/runner.js
+++ packages/lambdify-runner/src/runner.js
@@ -2,18 +2,21 @@ import { json } from './response';
 import payload from './payload';
 import request from './request';
 
+const buildResponsePayload = (response) => {
+	if (!response) {
+		return json(payload({}));
+	} else if (response.body || response.headers || response.statusCode) {
+		return response;
+	}
+	return json(payload(response));
+};
+
 export const runner = (event, context, fn) => {
 	new Promise(async (resolve) => {
 		try {
-			const responseObject = await fn(request(event, context));
+			const response = await fn(request(event, context));
 
-			if (!responseObject) {
-				resolve(json(payload({})));
-			} else if (responseObject.body || responseObject.headers || responseObject.statusCode) {
-				resolve(responseObject);
-			} else {
-				resolve(json(payload(responseObject)));
-			}
+			resolve(buildResponsePayload(response));
 		} catch (error) {
 			console.log(error);
 			resolve(json(payload({}, error)));
