2df7ab4071832a59794aa041b226d03e78f58613
William <william.h.giles@gmail.com>
2018-04-19 07:30:08 -0500

    updating payload to handle custom errors

--- packages/lambdify-runner/src/payload.js
+++ packages/lambdify-runner/src/payload.js
@@ -1,10 +1,24 @@
-export const payload = (body, error) => ({
-	debug: error ? error.stack : '',
-	message: error ? error.message : '',
-	name: process.env.AWS_LAMBDA_FUNCTION_NAME,
-	payload: body,
-	status: error ? 'error' : 'success',
-	timestamp: Math.floor(Date.now() / 1000),
-});
+export const payload = (body, error) => {
+	const response = {
+		debug: error ? error.stack : '',
+		message: error ? error.message : '',
+		name: process.env.AWS_LAMBDA_FUNCTION_NAME,
+		payload: body,
+		status: error ? 'error' : 'success',
+		timestamp: Math.floor(Date.now() / 1000),
+	};
+
+	if (error && error.code) {
+		response.error = {
+			code: error.code,
+			message: error.message,
+			name: error.name,
+			statuscode: error.statusCode,
+			type: error.type,
+		};
+	}
+
+	return response;
+};
 
 export default payload;
--- packages/lambdify-runner/tests/runner.test.js
+++ packages/lambdify-runner/tests/runner.test.js
@@ -16,6 +16,19 @@ test('payload loads object', () => {
 	expect(output.payload).toEqual(response);
 });
 
+test('payload loads custom error object', () => {
+	const output = runner.payload(response, {
+		code: 'NOT_FOUND',
+		logLevel: 'error',
+		message: 'Failed to find resource',
+		name: 'NotFoundError',
+		statusCode: 404,
+		type: 'NOT_FOUND',
+	});
+
+	expect(output.error.code).toEqual('NOT_FOUND');
+});
+
 test('request loads event', () => {
 	const req = runner.request(response);
 
