c14ed9d5331a30ee451a32e5b2eb943a36791bfe
William <william.h.giles@gmail.com>
2018-05-24 18:04:28 -0500

    better error handling and put a name if not on lambda

--- packages/lambdify-runner/src/payload.js
+++ packages/lambdify-runner/src/payload.js
@@ -2,20 +2,14 @@ export const payload = (body, error) => {
 	const response = {
 		debug: error ? error.stack : '',
 		message: error ? error.message : '',
-		name: process.env.AWS_LAMBDA_FUNCTION_NAME,
+		name: process.env.AWS_LAMBDA_FUNCTION_NAME || 'no_name',
 		payload: body,
 		status: error ? 'error' : 'success',
 		timestamp: Math.floor(Date.now() / 1000),
 	};
 
-	if (error && error.code) {
-		response.error = {
-			code: error.code,
-			message: error.message,
-			name: error.name,
-			statuscode: error.statusCode,
-			type: error.type,
-		};
+	if (error) {
+		response.error = JSON.parse(JSON.stringify(error));
 	}
 
 	return response;
