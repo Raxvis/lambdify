d0c8ccf2572b73475c8a715d70f2ce4cb94e6908
William <william.h.giles@gmail.com>
2018-05-24 18:07:19 -0500

    we always have a path so no or statement needed

--- packages/lambdify-runner/src/request.js
+++ packages/lambdify-runner/src/request.js
@@ -1,4 +1,4 @@
-const get = (record, path, defaultValue) => ((path.match(/([^.[\]]+)/g) || []).reduce((result, piece) => (result && result[piece]), record) || defaultValue);
+const get = (record, path, defaultValue) => ((path.match(/([^.[\]]+)/g)).reduce((result, piece) => (result && result[piece]), record) || defaultValue);
 
 const parseJSON = (json) => {
 	try {
