d57741d0f359bdce423f12df241cfc8fbd736cea
William <william.h.giles@gmail.com>
2018-05-30 10:34:08 -0500

    overhaul of build system to be centralized

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const babel = require('gulp-babel');
 const eslint = require('gulp-eslint');
 const eslintIfFixed = require('gulp-eslint-if-fixed');
 const gulp = require('gulp');
+const through = require('through2');
 const gulpif = require('gulp-if');
 const sourcemaps = require('gulp-sourcemaps');
 
@@ -9,16 +10,23 @@ const isJS = (file) => file.path.split('.').pop() === 'js';
 
 gulp.task('build', () =>
 	gulp
-		.src('src/**/*.js')
+		.src('packages/*/src/**/*.js')
 		.pipe(gulpif(isJS, eslint({ fix: true })))
 		.pipe(eslint.format())
 		.pipe(gulpif(isJS, eslintIfFixed('src')))
 		.pipe(sourcemaps.init())
 		.pipe(babel())
+		.pipe(
+			through.obj((file, enc, callback) => {
+				file.originalPath = file.path;
+				file.path = file.path.replace('src', 'dist');
+				callback(null, file);
+			}),
+		)
 		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('dist')),
+		.pipe(gulp.dest('packages')),
 );
 gulp.task('default', ['build']);
 gulp.task('watch', () => {
-	gulp.watch('src/**/*.js', ['build']);
+	gulp.watch('packages/*/src/**/*.js', ['build']);
 });
