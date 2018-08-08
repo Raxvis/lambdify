9deed006cacdb71d3292990e21460852a4d347a1
William <william.h.giles@gmail.com>
2018-08-08 18:47:48 -0500

    adding codecov

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
+	collectCoverage: true,
 	collectCoverageFrom: ['packages/*/src/**.{js,jsx}', '!**/node_modules/**', '!**/vendor/**'],
-	coverageDirectory: 'coverage',
+	coverageDirectory: './coverage/',
 	coverageReporters: ['lcov'],
 };
