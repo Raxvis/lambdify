e5a3eb1a3da69cc7a3d27e931887cff00d13d03d
William <william@prefinem.com>
2019-01-06 17:22:53 -0600

    fixing small init bug

--- src/lib/init.js
+++ src/lib/init.js
@@ -1,5 +1,5 @@
 module.exports = async (req, res, next) => {
 	const response = await next(req, res);
 
-	return response.getResponse ? response.getResponse() : response;
+	return response && response.getResponse ? response.getResponse() : response;
 };
