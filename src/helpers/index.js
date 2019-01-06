185be75e811ac3df2455ab63ad60ca25aff76151
William <william@prefinem.com>
2019-01-06 17:35:40 -0600

    can't import file that doesn't exist

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,12 +2,10 @@ const event = require('./event');
 const invoke = require('./invoke');
 const parsePayload = require('./parsePayload');
 const payload = require('./payload');
-const response = require('./response');
 
 module.exports = {
 	event,
 	invoke,
 	parsePayload,
 	payload,
-	response,
 };
