7900f2b4ac575e18763eb214437e749e1b63f5df
William <william@prefinem.com>
2019-01-10 14:21:25 -0600

    fixes #45 - handle encoding is binary

--- src/lib/helpers/html.js
+++ src/lib/helpers/html.js
@@ -1,4 +1,6 @@
+const encode = require('./encode');
+
 module.exports = (res, body) => {
 	res.headers = { ...res.headers, 'Content-Type': 'text/html' };
-	res.body = `${body}`;
+	res.body = res.isBase64Encoded ? encode(body) : body;
 };
--- src/lib/helpers/json.js
+++ src/lib/helpers/json.js
@@ -1,4 +1,6 @@
+const encode = require('./encode');
+
 module.exports = (res, body) => {
 	res.headers = { ...res.headers, 'Content-Type': 'application/json' };
-	res.body = JSON.stringify(body);
+	res.body = res.isBase64Encoded ? encode(JSON.stringify(body)) : JSON.stringify(body);
 };
