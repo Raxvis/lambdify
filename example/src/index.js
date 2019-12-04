d5635ff7bef8c85d6afcfd2b525092e3c9f81640
William <william@prefinem.com>
2019-12-03 19:27:04 -0600

    remove example in favor of docs

--- example/src/index.js
+++
@@ -1,9 +0,0 @@
-const lambdify = require('lambdify/src');
-
-const run = (request, response) => {
-	response.html(`<h1>Hello World</h1>`);
-
-	return response;
-};
-
-exports.handler = lambdify(run);
