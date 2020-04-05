8da3130b26818f8712b4c2a5d57277fd199f330b
William <william@prefinem.com>
2020-04-05 15:06:44 -0500

    collection coverage from src folder

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	collectCoverage: true,
-	collectCoverageFrom: ['helpers/**/*.js', 'lib/**/*.js', 'router/**/*.js', '!**/node_modules/**', '!**/vendor/**'],
+	collectCoverageFrom: ['src/**/*.js', '!**/node_modules/**', '!**/vendor/**'],
 	coverageDirectory: './coverage/',
 	coverageReporters: ['lcov'],
 };
