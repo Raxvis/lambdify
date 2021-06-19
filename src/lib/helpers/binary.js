143fc682d22739c683a9c7f30f191db0b6ef6c51
William <william@prefinem.com>
2021-06-19 12:27:21 -0500

    fixes #401

--- src/lib/helpers/binary.js
+++ src/lib/helpers/binary.js
@@ -1,5 +1,7 @@
+const encode = require('./encode');
+
 module.exports = (res, body, contentType) => {
   res.setBinaryResponse(true);
   res.setHeader('Content-Type', contentType);
-  res.setBody(body);
+  res.setBody(Buffer.isBuffer(body) ? encode(body) : body);
 };
